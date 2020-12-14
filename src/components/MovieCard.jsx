9f02ced654e7ce70406fd2c7da96e6c507d64e57
Darshan <dhulswar591@gmail.com>
2020-12-14 13:52:28 +0530

    styling movie component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,12 +4,14 @@ import "./MovieCard.css";
 export default function MovieCard(props) {
   return (
     <div className="movie-card">
-      {props.movie.title}
       <img
         className="movie-img"
         src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${props.movie.poster_path}`}
         alt=""
       />
+
+      <h1 className="movie-title">{props.movie.title}</h1>
+      <p>{props.movie.overview}</p>
     </div>
   );
 }
